49007c7db3c6b1727b86437283455a9940913e28
kajichul.chon@gmail.com <kajichul.chon@gmail.com>
2023-08-30 14:15:31 +0700

    done setting

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,8 @@ import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
-
+import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
 export default [
   {
     input: "src/index.ts",
@@ -20,11 +21,12 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }), postcss(), terser()],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/, { react: "react", "react-dom": "react-dom", "prop-types": "prop-types" }],
   },
 ];
